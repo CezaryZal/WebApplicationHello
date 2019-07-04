81b4487edfbefc18b9e2c2ceeeba7be33fff94e3
Cezary Zalewski <czarek0821@gmail.com>
2019-07-04 23:11:37 +0200

    add Servlet with config local server Jetty

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -1,15 +1,43 @@
 package io.github.mat3e;
 
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.*;
 import org.slf4j.LoggerFactory;
 
 import java.util.logging.Logger;
 
-/**
- * Hello world!
- */
 public class App {
-    public static void main(String[] args) {
-        var logger = LoggerFactory.getLogger(App.class);
-        logger.info("Hello World");
+    public static void main(String[] args) throws Exception {
+        var webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[]
+                {
+                        new AnnotationConfiguration(),
+                        new WebInfConfiguration(),
+                        new WebXmlConfiguration(),
+                        new MetaInfConfiguration(),
+                        new FragmentConfiguration(),
+                        new EnvConfiguration(),
+                        new PlusConfiguration(),
+                        new JettyWebXmlConfiguration()
+                });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+        //daje do zrozumienia jetty że skanowane przez niego klasy są na tym samym poziomie, gdzie wynikowy .jar
+
+        var server = new Server(8083);
+        server.setHandler(webapp);
+
+        server.start();
+        server.join();
+        //Jetty embedded – używanie jetty jak zwykłych klas javowych
+        // (https://www.eclipse.org/jetty/documentation/9.4.19.v20190610/maven-and-jetty.html).
+
+//        var logger = LoggerFactory.getLogger(App.class);
+//        logger.info("Hello World");
+        // uruchominie servera lokalnego jetty z konsoli, przez pluginy mavena. (plik pom.xlm) `jetty-maven-plugin`
     }
 }
---
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -0,0 +1,22 @@
+package io.github.mat3e;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "hello", urlPatterns = "/api/*") //gwiazdka umożliwia odpalenie `hello` nie zależnie co jest dalej
+public class HelloServlet extends HttpServlet {
+    private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Request got");
+        resp.getWriter().write("hello");
+    }
+}
