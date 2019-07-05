9f3dc5e7e436dce2007d8cf776ff2472adfb403d
Cezary Zalewski <czarek0821@gmail.com>
2019-07-05 11:44:15 +0200

    add constructor and method getParameter

--- src/main/java/io/github/mat3e/HelloServlet.java
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -9,14 +9,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "hello", urlPatterns = "/api/*") //gwiazdka umożliwia odpalenie `hello` nie zależnie co jest dalej
 public class HelloServlet extends HttpServlet {
+    private static final String NAME_PARAM = "name";
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+
+    private HelloService service;
+
+    /**
+     *  Servlet container needs it.
+     */
+    @SuppressWarnings("unused")
+    public HelloServlet (){
+        this(new HelloService());
+    }
+
+    HelloServlet (HelloService service){
+        this.service = service;
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("Request got");
-        resp.getWriter().write("hello");
+        logger.info("Request with parameters " + req.getParameterMap());
+
+        var name = req.getParameter(NAME_PARAM);
+        var greeting = service.prepareGreeting(name);
+        resp.getWriter().write(greeting);
+
+        //resp.getWriter().write(service.prepareGreeting(req.getParameter(NAME_PARAM)));
     }
 }
+
+
